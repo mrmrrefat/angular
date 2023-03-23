b988af137af59ecfda3d4f7daa67d7827c31bef3
marina refat <marinarefat0@gmail.com>
2023-03-23 21:58:38 +0200

    nav,sidebar,asidebar,footer

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { ProductsComponent } from './products/products.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsidebarComponent } from './asidebar/asidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    ProductsComponent
+    ProductsComponent,
+    SidebarComponent,
+    FooterComponent,
+    AsidebarComponent,
+    TopbarComponent
   ],
   imports: [
     CommonModule,
