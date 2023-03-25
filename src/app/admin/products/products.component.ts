0528e8163cae49aa87e7295307d309e9e0aa4c1a
nermeen <nermeenhabeel@gmail.com>
2023-03-25 18:48:56 +0200

    product list done

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { productModel } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+declare var $:any;
+declare var toastr:any;
+declare var toast:any;
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: productModel[] = [];
+  selectedImage!:File;
+constructor( private proSer:ProductService){
 
+}
+ngOnInit(): void {
+  this.proSer.AllProducts.subscribe(res=>{
+    this.products=res
+    console.log(this.products)
+  });
+}
+search(input:string){
+  // alert(input);
+  this.proSer.getFromDb(input);
+}
+onSelect(event: any){
+  var tmppath =URL.createObjectURL(event.target.files[0]);
+  $("#AddEmpImage").fadeIn("fast").attr('src',tmppath);
+  this.selectedImage=<File>event.target.files[0];
+
+}
 }
