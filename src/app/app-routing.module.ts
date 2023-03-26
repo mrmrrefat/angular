19356ffb37751d0f56365b90ab435f062d9b957f
Mahmoud Magdy <103686429+Mahmoud-Magdy17@users.noreply.github.com>
2023-03-26 15:33:49 +0200

    add products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,4 +9,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+          
+ }
